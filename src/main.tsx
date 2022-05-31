6f4db37d7bc762cc699c0c0a1eba15783e7e58d4
Laurent <laurent.lucian@gmail.com>
2022-05-31 01:56:50 -0700

    👾

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import theme from './libs/theme';
 import Particles from './libs/Particles';
-import Fonts from './libs/Fonts';
+import Fonts from './libs/fonts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
