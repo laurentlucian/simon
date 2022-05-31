e770faceff76ebc34977847e06fa23f054f92cff
Laurent <laurent.lucian@gmail.com>
2022-05-31 02:29:57 -0700

    👾

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePluginFonts } from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePluginFonts({
+      custom: {
+        families: [
+          {
+            name: 'MonoLisa',
+            local: 'MonoLisa',
+            src: './src/assets/MonoLisa-Regular.woff2',
+          },
+          {
+            name: 'MonoLisa Bold',
+            local: 'MonoLisa Bold',
+            src: './src/assets/MonoLisa-Bold.otf',
+          },
+        ],
+      },
+    }),
+  ],
 });
